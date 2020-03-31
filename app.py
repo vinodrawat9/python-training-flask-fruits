f5f7c2213e241f81b3037d601f941c5f448aad7f
Edison Garcia Chinas <edisga@microsoft.com>
2020-03-31 08:26:52 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, jsonify, render_template
+import os
+
+app = Flask(__name__)
+
+products = [
+    {
+        'id': 0,
+        'title': 'Apples',
+        'price': 1.20
+    },
+    {
+        'id': 1,
+        'title': 'Bananas',
+        'price': 1.45
+    },
+    {
+        'id': 2,
+        'title': 'Grapes',
+        'price': 5.12
+    },
+    {
+        'id': 3,
+        'title': 'Blackberries',
+        'price': 2.52
+    },
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html',title="Products", products=products)
+
+@app.route('/api/products/all', methods=['GET'])
+def get_products():
+    return jsonify(products)
+
+@app.route('/api/products', methods=['GET'])
+def get_products_byid():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Please specify a product id."
+
+    results=[]    
+
+    for product in products:
+        if product['id'] == id:
+            results.append(product)
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    port= os.environ.get('PORT')
+    app.run(host='0.0.0.0', debug=False, port=port)
